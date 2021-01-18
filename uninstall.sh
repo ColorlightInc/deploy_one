86e612efa9e77b0b0c3933f7ae0905d70995ec9a
jerry <jerrywong@lednets.com>
2021-01-18 19:34:17 +0800

    fix config

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-OUTPUT_DIR=clt_deploy
+OUTPUT_DIR=$(pwd)/clt_deploy
 echo "正在关闭服务..."
-cd ${OUTPUT_DIR} && docker-compose down && rm -rf ${OUTPUT_DIR}
-echo "正在删除clt_deploy部署目录..."
+if [ -d "$OUTPUT_DIR" ]; then
+    cd ${OUTPUT_DIR} && docker-compose down
+    echo "正在删除${OUTPUT_DIR}部署目录..."
+    rm -rf ${OUTPUT_DIR}
+fi
+
 echo "SUCCESS:colorlight cloud卸载完成"
