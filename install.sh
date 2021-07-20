82361860bb97d1dc023010ffe1ef74bd10acc78e
Jerry <jerrywong@lednets.com>
2021-07-20 11:44:33 +0800

    删除redis卷

--- install.sh
+++ install.sh
@@ -318,8 +318,7 @@ after_start_services() {
   chmod 600 -R ${OUTPUT_DIR}/nginx/logrotate && \
   chown -R ${COLORLIGHT_USER_UID}:${COLORLIGHT_GROUP_GID} /var/lib/docker/volumes/clt_deploy_nginx_log_data/_data && \
   chmod 777 -R /var/lib/docker/volumes/clt_deploy_spring_uploads_data/_data && \
-  chown -R ${COLORLIGHT_USER_UID}:${COLORLIGHT_GROUP_GID} /var/lib/docker/volumes/clt_deploy_spring_uploads_data/_data && \
-  chown -R ${COLORLIGHT_USER_UID}:${COLORLIGHT_GROUP_GID} /var/lib/docker/volumes/clt_deploy_one_redis_data/_data
+  chown -R ${COLORLIGHT_USER_UID}:${COLORLIGHT_GROUP_GID} /var/lib/docker/volumes/clt_deploy_spring_uploads_data/_data
 
   docker restart one-nginx >/dev/null 2>&1
 
---
+++ reset.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+sed -i '90c \    password: -MYSQL ENC-'  template/application.yml.template
